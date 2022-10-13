139011692c2c2c4e94b3c72b073e75c30125a966
Sandhya Ammasi <sandhyaammasi@gmail.com>
2022-10-13 09:29:20 -0500

    clientsocket edit

--- client.py
+++ client.py
@@ -6,3 +6,6 @@ PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname()) #ipconfig in cmd
 ADDR = (SERVER,PORT)
 FORMAT = 'utf-8'
+
+clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientsocket.connect()
