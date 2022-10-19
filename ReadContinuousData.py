c33f3a4eced09f0ec3184868aa5dffca184b5551
Sandhya Ammasi <sandhyaammasi@gmail.com>
2022-10-18 22:36:48 -0500

    reading continuous data edit 1

--- ReadContinuousData.py
+++ ReadContinuousData.py
@@ -1,5 +1,29 @@
 import time, socket
 
+def readUint32(array, arrayIndex):
+    variableBytes = array[arrayIndex : arrayIndex + 4]
+    variable = int.from_bytes(variableBytes, byteorder='little', signed=False)
+    arrayIndex = arrayIndex + 4
+    return variable, arrayIndex
+
+def readInt32(array, arrayIndex):
+    variableBytes = array[arrayIndex : arrayIndex + 4]
+    variable = int.from_bytes(variableBytes, byteorder='little', signed=True)
+    arrayIndex = arrayIndex + 4
+    return variable, arrayIndex
+
+def readUint16(array, arrayIndex):
+    variableBytes = array[arrayIndex : arrayIndex + 1]
+    variable = int.from_bytes(variableBytes, byteorder='little', signed=False)
+    arrayIndex = arrayIndex + 1
+    return variable, arrayIndex
+
+def readChar(array, arrayIndex):
+    variableBytes = array[arrayIndex : arrayIndex + 5]
+    variable = variableBytes.decode()
+    arrayIndex = arrayIndex + 5    
+    return variable,arrayIndex
+
 print('Connecting to TCP command server...')
 scommand = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 scommand.connect(('127.0.0.1', 5000))
@@ -22,8 +46,10 @@ time.sleep(0.1)
 scommand.sendall(b'set runmode run')
 time.sleep(2)
         
-#while True:
+while True:
         # Read spike data
-rawData = sSPK.recv(200000)
+        rawData = sSPK.recv(200000)
+        scommand.sendall(b'set runmode stop')
+
 
 print(rawData)
