0bc6144c537be8d9beed3987f31948eee33b2eac
Sandhya Ammasi <sandhyaammasi@gmail.com>
2022-11-16 04:13:58 -0600

    stimulus_data() individual func + glb Var'

--- RHXreadStoreSpike.py
+++ RHXreadStoreSpike.py
@@ -11,6 +11,7 @@ import numpy as np
 from collections import defaultdict
 import random
 from statistics import mean
+from send_stim_condition import stimulus_data
 def readUint32(array, arrayIndex):
     variableBytes = array[arrayIndex : arrayIndex + 4]
     variable = int.from_bytes(variableBytes, byteorder='little', signed=False)
@@ -37,6 +38,8 @@ def readChar(array, arrayIndex):
 
 
 def SpikeDataPerTrial(inputChannelArray,stim_cond):
+
+    scommand.sendall(b'set runmode run')
     channelDict = {channel:[] for channel in inputChannelArray}
 
     
@@ -46,16 +49,15 @@ def SpikeDataPerTrial(inputChannelArray,stim_cond):
         tcpCommandSPKchannel = tcpCommandSPKchannel.encode("utf-8")
         scommand.sendall(tcpCommandSPKchannel)
 
-    time.sleep(0.1)
 
-    scommand.sendall(b'set runmode run')
-    time.sleep(0.5) # run for 500ms - 0.5s
+    # run for 500ms - 0.5s
+    time.sleep(0.5) 
 
     rawData = sSPK.recv(200000)
     #print(rawData)
     
     scommand.sendall(b'set runmode stop')
-    time.sleep(0.1)
+    #time.sleep(0.1)
 
     spikeBytesPerBlock = 14
 
@@ -119,7 +121,7 @@ def plotSPKvsCHNL(channelDict,trialCount):
 
         fig.canvas.draw()
         fig.canvas.flush_events()
-        time.sleep(0.05)  
+        time.sleep(0.1)  
 
 def plotSPKvsSTIM(stim_cond,SPKcount): #x = stim_cond  y = SPKcount (int)
     
@@ -139,143 +141,151 @@ def plotSPKvsSTIM(stim_cond,SPKcount): #x = stim_cond  y = SPKcount (int)
     fig2.canvas.flush_events()
     time.sleep(0.1)
 
+def setup_TCPconnection():    
+    # TCP connection setup
+    print('Connecting to TCP command server...')
+    global scommand
+    scommand = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    scommand.connect(('127.0.0.1', 5000))
+
+    print('Connecting to TCP waveform server...')
+    global sSPK
+    sSPK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sSPK.connect(('127.0.0.1', 5002))
+
+    scommand.sendall(b'get runmode')
+    time.sleep(0.1)
+
+    scommand.sendall(b'get sampleratehertz')
+    time.sleep(0.1)
+
+    scommand.sendall(b'execute clearalldataoutputs')
+    time.sleep(0.1)
+
     
-# TCP connection
-print('Connecting to TCP command server...')
-scommand = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-scommand.connect(('127.0.0.1', 5000))
+if __name__ == '__main__':
+    # setting up plot 
+    print("opening plot......")
+    plt.ion() # Enable interactive mode for plot
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2,figsize=(10, 10))
+    axes = [ax1,ax2,ax3,ax4] 
+    # fig, axes = plt.subplots(nX,nY)
 
-print('Connecting to TCP waveform server...')
-sSPK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sSPK.connect(('127.0.0.1', 5002))
+    #plot set up for 4 channels and SPKs
+    xyArr = [([],[]),([],[]),([],[]),([],[])] #for 4 channels (if > 4 Channels , append ([],[]) - note: it should be a tuple)
+    sc = [] # scatter plot list
+    for axis,i in zip(axes,range(len(axes))):
+        sc.append(axis.scatter([],[],marker='|'))
+        plt.setp(axis, xlim=(0,550), ylim=(0,5))
+    fig.suptitle('SPIKE Data for channels ')
+    fig.text(0.5, 0.04, 'Timestamps', ha='center', va='center')
+    fig.text(0.06, 0.5, 'Trials', ha='center', va='center', rotation='vertical')
 
-scommand.sendall(b'get runmode')
-time.sleep(0.1)
-
-scommand.sendall(b'get sampleratehertz')
-time.sleep(0.1)
-
-scommand.sendall(b'execute clearalldataoutputs')
-time.sleep(0.1)
-
-print("opening plot......")
-# setting up plot 
-plt.ion() # Enable interactive mode for plot
-
-fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2,figsize=(10, 10))
-axes = [ax1,ax2,ax3,ax4] 
-# lauri : customize subplot according to channels 
-# fig, axes = plt.subplots(nX,nY)
-
-#plot set up for 4 channels and SPKs
-xyArr = [([],[]),([],[]),([],[]),([],[])] #for 4 channels (if > 4 Channels , append ([],[]) - note: it should be a tuple)
-sc = [] # scatter plot list
-for axis,i in zip(axes,range(len(axes))):
-    sc.append(axis.scatter([],[],marker='|'))
-    plt.setp(axis, xlim=(0,550), ylim=(0,5))
-fig.suptitle('SPIKE Data for channels ')
-fig.text(0.5, 0.04, 'Timestamps', ha='center', va='center')
-fig.text(0.06, 0.5, 'Trials', ha='center', va='center', rotation='vertical')
-
-plotSPKvsSTIM_y = []
-#plot setup for number of spikes and stim_conditions
-x1 = [str()]
-#y1 = np.array([None])  initializing an empty numpy array
-y1 =[None] # initialzing with None since x1 is initialized with empty string list
-fig2, ax = plt.subplots(figsize=(8, 8))
-line1, = ax.plot(x1, y1,'-o')
-plt.setp(ax, xlim=(0,6), ylim=(0,25)) #xlim = unique_stim_conditions++
-fig2.suptitle('No. of SPK vs Stimulus conditions')
-fig2.text(0.5, 0.04, 'Stimulus conditions', ha='center', va='center')
-fig2.text(0.06, 0.5, 'count(SPK)', ha='center', va='center', rotation='vertical')
-#ax.set_xticklabels(['a','b','c','d','e','f'])
+    #plot setup for number of spikes and stim_conditions
+    x1 = [str()]
+    #y1 = np.array([None])  initializing an empty numpy array
+    y1 =[int()] # initialzing with None since x1 is initialized with empty string list
+    fig2, ax = plt.subplots(figsize=(8, 8))
+    line1, = ax.plot(x1, y1,'-o')
+    ax.set_xlim(0,6) #xlim = unique_stim_conditions++
+    ax.set_ylim(0,50)
+    fig2.suptitle('No. of SPK vs Stimulus conditions')
+    fig2.text(0.5, 0.04, 'Stimulus conditions', ha='center', va='center')
+    fig2.text(0.06, 0.5, 'count(SPK)', ha='center', va='center', rotation='vertical')
+    #ax.set_xticklabels(['a','b','c','d','e','f'])
 
 
-# setting up list/array to store timestamps , channel list as input , stimulus conditions
-totTimeStampsList = []
-userIPchannels = ["A-001","A-002","A-003","A-004"]
-plotSPKvsSTIM_xy = {}
-tot_stim_condition = ['a','c','b','d','e','a','d','c','b','e','d','e','b','c','a','e','a','c','d','b']
-#tot_stim_condition = ['a','c','b','c','a','b']
-unique_stim_conditions = len(list(set(tot_stim_condition)))
-stimulusComp_Inp = True
-no_of_trials = len(tot_stim_condition)
+    # setting up list/array to store timestamps , channel list as input , stimulus conditions
+    totTimeStampsList = []
+    userIPchannels = ["A-001","A-002","A-003","A-004"]
+    plotSPKvsSTIM_xy = {}
+    unique_stim_conditions = len(list(set(stimulus_data())))
+    stimulusComp_Inp = True
+    no_of_trials = len(stimulus_data())
 
-stim_SPK_Count = {}
-data = np.empty((0,unique_stim_conditions)) # 5 => unique(tot_Stim_condition)
+    stim_SPK_Count = {}
+    data = np.empty((0,unique_stim_conditions)) # 5 => unique(tot_Stim_condition)
 
-#stim_trial_array = np.nan * np.ones((2000,stimulus_conditions,len(userIPchannels)))
+    #stim_trial_array = np.nan * np.ones((2000,stimulus_conditions,len(userIPchannels)))
 
-while stimulusComp_Inp:
+
+    setup_TCPconnection()
+
+    while stimulusComp_Inp:
+
+        # note : trial1 => stim_cond1
+        #        trial2 => stim_cond2   etc
+        for tr,stim_cond in zip(range(1,no_of_trials+1),stimulus_data()):
     
-    # note : trial1 => stim_cond1
-    #        trial2 => stim_cond2   etc
-    for tr,stim_cond in zip(range(1,no_of_trials+1),tot_stim_condition):
- 
-        channelDict,stim_SPK_Count = SpikeDataPerTrial(userIPchannels,stim_cond)
-        if (tr)%unique_stim_conditions == 0:
-            data = np.append(data,np.array([list(stim_SPK_Count.values())]),axis = 0)
-   
-        totTimeStampsList.append(channelDict)
-        plotSPKvsCHNL(channelDict,tr+1) 
-        spikeCount = 0
-        for tsArr in channelDict.values(): #tsArr : time stamp Array
-            spikeCount+=len(tsArr)            
+            channelDict,stim_SPK_Count = SpikeDataPerTrial(userIPchannels,stim_cond)
 
-        plotSPKvsSTIM(stim_cond,spikeCount)
-
-        #stim_trial_array[rep,stim_cond,:] = spkC        
-        #mn = np.nanmean(stim_trial_array[rep,stim_cond,:],axis=0)       
-
-
-        #numpy array - no.of spikes vs stim_cond
-        # realtime plot after every trial
-        # Lauri : save number of spikes in multi dimensional array
-
-    totTimeStamps = defaultdict(list) # keys : channels , values : [timestamps]
-
-    for eachtrial in (totTimeStampsList): # you can list as many input dicts as you want here
-        for key, value in eachtrial.items():
-            totTimeStamps[key].extend(value)
+            if (tr)%unique_stim_conditions == 0:
+                data = np.append(data,np.array([list(stim_SPK_Count.values())]),axis = 0)
     
+            totTimeStampsList.append(channelDict)
+            plotSPKvsCHNL(channelDict,tr) 
+            spikeCount = 0
+            for tsArr in channelDict.values(): #tsArr : time stamp Array
+                spikeCount+=len(tsArr)            
+
+            plotSPKvsSTIM(stim_cond,spikeCount)
+
+            #stim_trial_array[rep,stim_cond,:] = spkC        
+            #mn = np.nanmean(stim_trial_array[rep,stim_cond,:],axis=0)       
+
+
+            #numpy array - no.of spikes vs stim_cond
+            # realtime plot after every trial
+            # Lauri : save number of spikes in multi dimensional array
+
+        totTimeStamps = defaultdict(list) # keys : channels , values : [timestamps]
+
+        for eachtrial in (totTimeStampsList): # you can list as many input dicts as you want here
+            for key, value in eachtrial.items():
+                totTimeStamps[key].extend(value)
+        
+        
+
+        plt.figure(3)
+        palette = sns.color_palette("dark:violet")
+        plt.bar(totTimeStamps.keys(),[len(totTimeStamps[key]) for key in totTimeStamps.keys()],color=palette)
+        plt.title("no. of spikes vs Channel")
+        plt.ylabel("count(SPK)")
+
+
+        stimulus_cond = [data[:,i] for i in range(len(data)+1)]
+        fig3, axes = plt.subplots()
+        fig3.suptitle('No. of SPK vs Stimulus conditions')
+        axes.boxplot(stimulus_cond,showmeans=True)
+
+        #plt.show()
+        user_input = input("Enter 'q' to quit: ")
+        if user_input == 'q':
+            print(data)
+            print(stimulus_cond)
+            break
+
+    # Lauri Pseudo codes
+    '''
+    #scommand.sendall(b'set runmode run')
+
+    while True:
+        # wait for stimulus computer to tell when stimuli is on the screen
+
+        # start collecting spikes from Intan
+        channelDict = SpikeDataPerTrial(["A-000","A-001","A-002","A-003"])
+
+        # update plot using the code snippets below
+
+    # open plots
     plt.figure(1)
-    palette = sns.color_palette("dark:violet")
-    plt.bar(totTimeStamps.keys(),[len(totTimeStamps[key]) for key in totTimeStamps.keys()],color=palette)
-    plt.title("no. of spikes vs Channel")
-    plt.ylabel("count(SPK)")
-
-    plt.figure(2)
-    stimulus_cond = [data[:,i] for i in range(len(data)+1)]
-    fig3, axes = plt.subplots()
-    axes.boxplot(stimulus_cond,showmeans=True)
-    
-   
-    user_input = input("Enter 'q' to quit: ")
-    if user_input == 'q':
-        print(data)
-        break
-
-# Lauri Pseudo codes
-'''
-#scommand.sendall(b'set runmode run')
-
-while True:
-    # wait for stimulus computer to tell when stimuli is on the screen
-
-    # start collecting spikes from Intan
-    channelDict = SpikeDataPerTrial(["A-000","A-001","A-002","A-003"])
-
-    # update plot using the code snippets below
-
-# open plots
-plt.figure(1)
-indx = 0
-while not stopped:
-    channelDict = SpikeDataPerTrial(["A-000","A-001","A-002","A-003"])
-    if indx == 0:
-        # line handle
-    increment 
-    update handle        
+    indx = 0
+    while not stopped:
+        channelDict = SpikeDataPerTrial(["A-000","A-001","A-002","A-003"])
+        if indx == 0:
+            # line handle
+        increment 
+        update handle        
 
 
-'''
+    '''
---
+++ send_stim_condition.py
@@ -0,0 +1,6 @@
+def stimulus_data():
+    tot_stim_condition = ['a','e','c','b','d','c','d','a','b','e']
+    return tot_stim_condition
+
+    #tot_stim_condition = ['a','c','b','d','e','a','d','c','b','e','d','e','b','c','a','e','a','c','d','b']
+    #tot_stim_condition = ['a','c','b','c','a','b']
